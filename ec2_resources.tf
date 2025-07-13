cf1fbcdb78b6df1d41e724b1ff92b2363a418fae
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-13 19:23:21 +0100

    security group and key for ec2

--- ec2_resources.tf
+++ ec2_resources.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "ec2_subnet_1" {
   ami           = "ami-0eb9d6fc9fab44d24"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.security_group_web_access.id, aws_security_group.security_group_ssh_access.id]
+  key_name = aws_key_pair.key_pair.key_name
   user_data = <<-EOF
         #!/bin/bash
         sudo yum update -y
@@ -17,3 +19,27 @@ resource "aws_instance" "ec2_subnet_1" {
   }
 }
 
+resource "aws_instance" "ec2_subnet_2" {
+  ami           = "ami-0eb9d6fc9fab44d24"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnet_2.id
+  vpc_security_group_ids = [aws_security_group.security_group_web_access.id, aws_security_group.security_group_ssh_access.id]
+  key_name = aws_key_pair.key_pair.key_name
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo yum update -y
+        sudo amazon-linux-extras install nginx1 -y 
+        sudo systemctl enable nginx
+        sudo systemctl start nginx
+        EOF 
+  
+
+  tags = {
+    Name = "ec2_subnet_2"
+  }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "project_key"
+  public_key = file("/Users/bushrafatima/.ssh/project_key.pub")
+}
