f07084d9ab4c0bf5f0f0902ef145f0fdd0b8d929
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-13 18:49:01 +0100

    security groups for ec2

--- security_groups.tf
+++ security_groups.tf
@@ -36,3 +36,46 @@ resource "aws_security_group" "securitygroup_loadbalancer" {
   }
   
 }
+
+// security group for ec2 instance 
+
+resource "aws_security_group" "security_group_web_access" {
+  name        = "security group for ec2 web access"
+  description = "Allow HTTP/HTTPS access to web server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "security_group_ssh_access" {
+  name        = "security group for ec2 ssh access"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
