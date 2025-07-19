364c31350309bb05b3d96da2b5c9d7fe21db7e67
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-19 20:40:30 +0100

    removed unnecessary file

--- rds_instance.tf
+++ rds_instance.tf
@@ -2,9 +2,11 @@
 resource "aws_db_instance" "rds" {
   engine                 = "mysql"
   identifier             = "myrdsinstance"
-  allocated_storage      = 5
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  allocated_storage      = 10
+  storage_type           = "gp2"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  parameter_group_name = "default.mysql8.0"
   username               = var.dbs_username
   password               = var.dbs_password
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet.name
--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install nginx -y
-echo "Hi Bushra" >/var/www/html/index.nginx-debian.html
