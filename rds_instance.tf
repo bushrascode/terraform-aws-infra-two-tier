f25600d4323924588e08fba9062abd9210ba5d96
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-19 15:38:29 +0100

    rds referencing variables

--- rds_instance.tf
+++ rds_instance.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "rds" {
   allocated_storage    =  5
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  username             = ""
-  password             = ""
+  username             = var.dbs_username
+  password             = var.dbs_password
   db_subnet_group_name = aws_db_subnet_group.rds_subnet
   vpc_security_group_ids = [aws_security_group.db_securitygroup.id]
   skip_final_snapshot  = true
