c122608150a3274a6b9f2ffda5e9e5cab981ac93
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-19 15:50:04 +0100

    fixed db subnet group name

--- rds_instance.tf
+++ rds_instance.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "rds" {
   instance_class       = "db.t2.micro"
   username             = var.dbs_username
   password             = var.dbs_password
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
   vpc_security_group_ids = [aws_security_group.db_securitygroup.id]
   skip_final_snapshot  = true
   publicly_accessible =  false
