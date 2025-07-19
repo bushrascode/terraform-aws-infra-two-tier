1f043360ae9d66b85bafd573079406086966002e
Bushra Fatima <bushrafatima23@gmail.com>
2025-07-19 15:28:40 +0100

    bare bones of rds instance

--- rds_instance.tf
+++ rds_instance.tf
@@ -1,14 +1,14 @@
 #create a RDS Database Instance
-# resource "aws_db_instance" "rds" {
-#   engine               = "mysql"
-#   identifier           = "myrdsinstance"
-#   allocated_storage    =  5
-#   engine_version       = "5.7"
-#   instance_class       = "db.t2.micro"
-#   username             = "myrdsuser"
-#   password             = "myrdspassword"
-#   parameter_group_name = "default.mysql5.7"
-#   vpc_security_group_ids = aws_security_group.rds_sg.id // need to create 
-#   skip_final_snapshot  = true
-#   publicly_accessible =  false
-# }
+resource "aws_db_instance" "rds" {
+  engine               = "mysql"
+  identifier           = "myrdsinstance"
+  allocated_storage    =  5
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = ""
+  password             = ""
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet
+  vpc_security_group_ids = [aws_security_group.db_securitygroup.id]
+  skip_final_snapshot  = true
+  publicly_accessible =  false
+}
